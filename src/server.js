9217a4446fc652aeded3a90d9bd58338e5ee2dae
Raghad-Mezied <rmezied3@gmail.com>
2021-08-04 00:12:32 +0300

    fix errors in server.js using eslin Relates #21

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
-const http=require("http");
-require("env2")(".env");
-const router=require("../src/router");
+const http = require('http');
+require('env2')('.env');
+const router = require('./router');
+
 const server = http.createServer(router);
 const PORT = process.env.PORT || 9000;
 
-server.listen(PORT,() => {
-    console.log(`server is runing at : http://localhost:${PORT}`);
+server.listen(PORT, () => {
+  console.log(`server is runing at : http://localhost:${PORT}`);
 });
